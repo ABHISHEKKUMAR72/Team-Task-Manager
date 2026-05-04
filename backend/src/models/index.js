e5a092eff0f9de19f08881f51d394fdc11ffcbf9
Abhishek Kumar <abhikgupta345@gmail.com>
2026-05-04 22:42:31 +0530

    Remove backend codebase including models, controllers, routes, middleware, and configuration files for the Team Task Manager API.

--- backend/src/models/index.js
+++
@@ -1,6 +0,0 @@
-import User from './User.js';
-import Project from './Project.js';
-import Task from './Task.js';
-import ProjectMember from './ProjectMember.js';
-
-export { User, Project, Task, ProjectMember };
